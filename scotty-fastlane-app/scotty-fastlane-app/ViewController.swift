771b6ad477c4cb40514dc641523735ab9fbb0f88
Scotty-Montgomery <scotty@youandthegang.com>
2016-06-10 15:32:22 +0200

    changed ignore

---
+++ scotty-fastlane-app/scotty-fastlane-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scotty-fastlane-app
+//
+//  Created by Scotty on 10/06/16.
+//  Copyright © 2016 you & the gang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
